4d45303878d0feeac333afe963fcacf34c5487ec
razorloves <razorloves@gmail.com>
2016-08-10 12:50:45 -0700

    shamu: update build fingerprint & description to MOB30W
    Change-Id: I90756b369b519fa9da0ed7c15e7a9fe63777d5d8
    
    Conflicts:
    	aosip.mk

--- aosip.mk
+++ aosip.mk
@@ -2,11 +2,8 @@
 TARGET_SCREEN_HEIGHT := 2560
 TARGET_SCREEN_WIDTH := 1440
 
-# Inherit some common tesla stuff.
+# Inherit some common aosip stuff.
 $(call inherit-product, vendor/aosip/config/common_full_phone.mk)
-
-# Enhanced NFC
-$(call inherit-product, vendor/aosip/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/moto/shamu/aosp_shamu.mk)
@@ -22,5 +19,5 @@ TARGET_VENDOR := moto
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=shamu \
-    BUILD_FINGERPRINT=google/shamu/shamu:6.0.1/MOB30M/2862625:user/release-keys \
-    PRIVATE_BUILD_DESC="shamu-user 6.0.1 MOB30M 2862625 release-keys"
+    BUILD_FINGERPRINT=google/shamu/shamu:6.0.1/MOB30W/3031100:user/release-keys \
+    PRIVATE_BUILD_DESC="shamu-user 6.0.1 MOB30W 3031100 release-keys"
