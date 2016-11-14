a096fc03788db4ded27463a1b8d6236703046139
REV3NT3CH <rev3nt3ch@gmail.com>
2016-11-13 19:58:32 -0800

    Add AOSiP dependencies and Welcome B14CKB1RD

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,9 +25,11 @@ ENABLE_CPUSETS := true
 TARGET_NO_BOOTLOADER := true
 
 # Inline kernel building
-TARGET_KERNEL_CONFIG := shamu_defconfig
+TARGET_KERNEL_CONFIG := B14CKB1RD_defconfig
 TARGET_KERNEL_SOURCE := kernel/moto/shamu
 BOARD_KERNEL_IMAGE_NAME := zImage-dtb
+KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-6.x/bin
 
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE :=  2048
